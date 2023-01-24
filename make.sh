2867ffc248e762385f91d647996c7ce2062b58b9
ShanghaiZhouWanLi <106662135+mi20210630@users.noreply.github.com>
2023-01-24 16:26:54 +0800

    Update make.sh

--- make.sh
+++ make.sh
@@ -165,12 +165,8 @@ $romsdir/$sourcever/$romtype/$romtypename/debloat.sh "$systemdir/system" 2>/dev/
 if [[ ! -e $romsdir/$sourcever/$romtype/$romtypename/DONTRESIGN ]]; then
     if [[ ! -e $romsdir/$sourcever/$romtype/DONTRESIGN ]]; then
         echo "Resigning to AOSP keys"
-        ispython2=`python -c 'import sys; print("%i" % (sys.hexversion<0x03000000))'`
-        if [ $ispython2 -eq 0 ]; then
-            python2=python2
-        else
-            python2=python
-        fi
+#        ispython2=`python -c 'import sys; print("%i" % (sys.hexversion<0x03000000))'`
+        python2=python
         $python2 $toolsdir/ROM_resigner/resign.py "$systemdir/system" $toolsdir/ROM_resigner/AOSP_security > $tempdir/resign.log
         $prebuiltdir/resigned/make.sh "$systemdir/system" 2>/dev/null
     fi
